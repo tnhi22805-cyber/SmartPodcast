4e8254d60df55ead9054bcd2831ab3a26a5e1075
Nhi <tnhi22805@gmail.com>
2026-04-05 19:14:38 +0700

    refactor: clean up duplicate files and organize package structure

--- app/src/main/java/com/example/smartpodcast/data/PodcastApi.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smartpodcast.data
-import retrofit2.http.GET
-import retrofit2.http.Url
-
-interface PodcastApi {
-    @GET
-    suspend fun getPodcastFeed(@Url url: String): Any // Sau này sẽ parse XML ở đây
-}
