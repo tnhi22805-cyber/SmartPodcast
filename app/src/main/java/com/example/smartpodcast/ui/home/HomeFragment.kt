e49b31030369812ac2ae2cd6fd25c83cd6d551c5
Hang <nguyenxhuong1105@gmail.com>
2026-04-11 15:01:39 +0700

    ui: redesign episode item to grid style and implement GridLayoutManager for 2-column display

--- app/src/main/java/com/example/smartpodcast/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/smartpodcast/ui/home/HomeFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.smartpodcast.R
@@ -76,7 +76,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }
 
         // 2. Setup RecyclerView
-        rvEpisodes.layoutManager = LinearLayoutManager(requireContext())
+        rvEpisodes.layoutManager = GridLayoutManager(requireContext(), 2)
         rvEpisodes.adapter = episodeAdapter
 
         // 3. Setup Mini Player
